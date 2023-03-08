45f111a3ebb5b9f8aad8b409a1e9b8419bb4768a
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-03-08 22:15:54 +0100

    start building up continuous integration

--- gerstnerhungariancommands/makein.py
+++ gerstnerhungariancommands/makein.py
@@ -18,16 +18,26 @@ def cln(w):
     #TODO: clean so vector coverage is higher
     return w
 
+def getoptyear():
+    #TODO, write this function
+    with open("raw/Gerstner-2016-10176.tsv", "r") as f:
+        for row in f.read().split("\n")[1:][:-1]:
+            row = row.split("\t")
+    return 1600
+
 def run(args):
     """
     Read values from forms.csv and IPA-transcribe the Spanish ones
     """
     hng = HNG()
-    #ds = Dataset.from_metadata("./cldf/cldf-metadata.json")
+
+    optyear = getoptyear()
     lines = "Segments\tMeaning\tEntry_ID"
     with open("raw/Gerstner-2016-10176.tsv", "r") as f:
         for i, row in enumerate(f.read().split("\n")[1:][:-1]):
             row = row.split("\t")
+            if row[2] and int(row[2]) > optyear:  # filter years
+                continue
             lines += f"\n{segipa(row[0])}\t{cln(row[1])}\t{str(i+1)}-{slug(row[0])}"
 
     # write csv
