06e959c7ffb75f724e33e79591c235fb2a535e80
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-09 01:18:59 +0200

    ci fix flags, add them to shell script

--- hun.sh
+++ hun.sh
@@ -1,1 +1,5 @@
-cldfbench lexibank.makecldf lexibank_gerstnerhungarian.py  --concepticon-version=v2.5.0 --glottolog-version=v4.5 --clts-version=v2.2.0
+cldfbench lexibank.makecldf lexibank_gerstnerhungarian.py  \
+--concepticon-version=v2.5.0 --glottolog-version=v4.5 --clts-version=v2.2.0 \
+--concepticon=/home/circleci/project/concepticon-data \
+--glottolog=/home/circleci/project/glottolog \
+--clts=/home/circleci/project/clts
