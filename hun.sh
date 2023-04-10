6932530a747e70c2ddf7cf7a612f9c9c91de5e67
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-04-10 16:36:43 +0200

    ci fix paths to reference catalogues in circleci workflow

--- hun.sh
+++ hun.sh
@@ -1,2 +1,5 @@
 cldfbench lexibank.makecldf lexibank_gerstnerhungarian.py  \
---concepticon-version=v2.5.0 --glottolog-version=v4.5 --clts-version=v2.2.0
+--concepticon-version=v2.5.0 --glottolog-version=v4.5 --clts-version=v2.2.0 \
+--concepticon=../concepticon/concepticon-data \
+--glottolog=../glottolog \
+--clts=../clts
