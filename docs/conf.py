6f25a41be59300759a89612af5276f6962ccaee1
Viktor Martinović <viktor_martinovic@eva.mpg.de>
2023-05-08 15:32:48 +0200

    [skip ci] addcopybutton

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+# https://sphinx-copybutton.readthedocs.io/en/latest/index.html
+
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'gerstnerhungarian'
@@ -10,9 +13,18 @@ copyright = '2023, Viktor Martinović'
 author = 'Viktor Martinović'
 version = '2.0'
 release = '2.0'
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_copybutton']
 html_theme = 'sphinx_rtd_theme'
-# TODO replace mock with docs/requirements.txt after loanpy-release
-autodoc_mock_imports = ["epitran", "loanpy", "lingpy", "clldutils", "tabulate",
-                        "attr", "pylexibank", "cldfbench", "spacy",
-                        "lexibank_gerstnerhungarian", "pycldf", "pysem"]
+
+# uncomment row below if problems with installation
+autodoc_mock_imports = [
+    "attr",
+    "cldfbench",
+    "clldutils",
+    "lexibank_gerstnerhungarian",
+    "pycldf",
+    "pylexibank",
+    "pysem",
+    "spacy",
+    "tabulate",
+]
